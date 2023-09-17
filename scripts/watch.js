fae8b190f30947b39bbf35bfc75ce07d4ac3f4bd
Zenology <zen.noppanan@gmail.com>
2023-09-17 14:26:32 +0700

    Ad auto deployment script

--- scripts/watch.js
+++ scripts/watch.js
@@ -9,19 +9,42 @@ if (!saveLocation) {
   throw new Error('save location not found: ' + saveLocation)
 }
 
-const watcher = chokidar.watch(path.resolve(saveLocation, 'new'))
+const srcPath = path.resolve(__dirname, '../src')
+const newTexturePath = path.resolve(saveLocation, 'new')
 
-watcher.on('ready', () => {
-  fs.ensureDirSync(path.resolve(__dirname, '../tmp'))
+const srcWatcher = chokidar.watch(srcPath)
 
-  console.log('[READY] - Folder Watched Ready: ' + path.resolve(saveLocation, 'new'))
+srcWatcher.on('ready', () => {
+  console.log('[READY] - Source Folder Watched Ready: ' + srcPath)
 })
 
-watcher.on('change', (pathName) => {
+srcWatcher.on('change', (pathName) => {
+  const fileName = path.basename(pathName)
+
+  if (!/X\d+ - /.test(fileName) && !/\.(psd|ai|tmp)$/.test(fileName) && !/^[A-Fa-f0-9]+\..+/.test(fileName)) {
+    const rootFolder = pathName.replace(srcPath + "\\", '').replace("\\" + fileName, '')
+
+    fs.ensureDirSync(path.resolve(saveLocation, rootFolder))
+    fs.cpSync(pathName, path.resolve(saveLocation, rootFolder, fileName))
+
+    console.info('[DEPLOY] Texture file to PPSSPP: ' + rootFolder + "/" + fileName)
+  }
+})
+
+const destWatcher = chokidar.watch(newTexturePath)
+
+destWatcher.on('ready', () => {
+  fs.ensureDirSync(path.resolve(__dirname, '../tmp'))
+
+  console.log('[READY] - Destination Folder Watched Ready: ' + newTexturePath)
+})
+
+destWatcher.on('change', (pathName) => {
   const fileName = path.basename(pathName)
 
   if (!fs.existsSync(path.resolve(__dirname, '../tmp', fileName))) {
     fs.cpSync(pathName, path.resolve(__dirname, '../tmp', fileName), { recursive: true })
-    console.info('[COPY] file: ' + fileName)
+    console.info('[SYNC] Texture file to src: ' + fileName)
   }
 })
+
